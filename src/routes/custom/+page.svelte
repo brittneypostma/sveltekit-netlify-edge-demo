d0caa40aa55512cd01a678ad3fee7bfedcc198e6
brittneypostma <bdesignedwebdev@gmail.com>
2022-10-20 07:46:47 -0400

    Initial commit

---
+++ src/routes/custom/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	async function getEdgeData() {
+		const response = await fetch('/location');
+
+		const geoData = await response.json();
+		return geoData;
+	}
+</script>
+
+<main>
+	<h1>Welcome to SvelteKit</h1>
+	<h2>Netlify Edge Geolocation data:</h2>
+	{#await getEdgeData()}
+		<section>
+			<p><strong>Location</strong>: ...</p>
+		</section>
+	{:then { city, country }}
+		<section>
+			<p><strong>Location</strong>: {city}, {country}</p>
+		</section>
+	{/await}
+</main>
