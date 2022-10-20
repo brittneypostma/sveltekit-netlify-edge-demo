32d22ec246e58798a22c16ef754f68cf8037f8d0
Brittney Postma <bdesignedwebdev@gmail.com>
2022-10-20 08:13:27 -0400

    typo ;

--- src/routes/edge/+page.server.ts
+++ src/routes/edge/+page.server.ts
@@ -5,6 +5,32 @@ export const load: PageServerLoad = async (event) => {
 	const geo = event.platform.context.geo
 	return {
 		city: geo.city,
-		region: geo.subDivision.name
+		region: geo.subdivision.name
 	};
 };
+/* 
+context: {
+  cookies: {
+    delete: [Function: bound delete],
+    get: [Function: bound get],
+    set: [Function: bound set]
+  },
+  geo: {
+    city: "Holland",
+    country: { code: "US", name: "United States" },
+    subdivision: { code: "MI", name: "Michigan" }
+  },
+  ip: "2600:6c4a:747f:a7c4:507c:6160:872d:7f77",
+  json: [Function: bound json],
+  log: [Function],
+  next: [Function: next],
+  requestId: "01GFTMAZHAHD5NJ26KT4H81HDF",
+  rewrite: [Function: bound rewrite],
+  site: {
+    id: "79fa37a2-7da8-4383-952b-bb5e1aadcf9d",
+    name: "statuesque-pika-5232b3",
+    url: "http://statuesque-pika-5232b3.netlify.app"
+  },
+  account: { id: "5d260577f54c73a4f797c5e1" }
+}
+*/
